2788feb857334903f945271c19713fd517043546
骏 冷 <Lelouch-LJ@hotmail.com>
2021-01-11 22:12:09 +0800

    2021.01.06_week17练习

---
+++ 18041615_LengJun/2021.01.06_week17/dip-class-demos-feature15.DeepNeuralNetwork/dip-class-demos/parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"E:\\Test_Image\\vtest.avi"		
+#define OPENPOSE_VIDEO		"E:\\Test_Image\\openposeTest.flv"		
+
+//#define INPUT_METHOD									//0: image 1:video 2:camera
